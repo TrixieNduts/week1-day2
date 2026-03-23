c1f27a9a4e88a2e09f48fbe75efc8431fecfc611
User <annabeat08@gmail.com>
2026-03-24 02:05:02 +0300

    Am loving this soo far

---
+++ Week 1_Exercise_4.py
@@ -0,0 +1,10 @@
+import sys
+
+def main():
+
+    number =int(input("Kindly enter any number = "))
+    print(f"The square of the number is = {number ** 2}")
+    print(f"The cube of the number is = {number ** 3}")
+
+if __name__ == "__main__":
+    sys.exit(main())
