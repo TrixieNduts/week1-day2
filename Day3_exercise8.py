081a1a125a3dad100b6f810f300a70503098c60c
User <annabeat08@gmail.com>
2026-03-25 01:07:16 +0300

    We can do this

---
+++ Day3_exercise8.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+
+    word1 = str(input("kindly input the first word, "))
+    word2= str(input("kindly input the second word, "))
+
+    print(f"{word1} {word2}.")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
