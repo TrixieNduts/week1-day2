198b4addd5046c02dda2f086c257015470f24dcf
User <annabeat08@gmail.com>
2026-03-26 02:02:48 +0300

    Converting mins to hours

---
+++ Day4_Exercice11.py
@@ -0,0 +1,12 @@
+import sys
+
+def main():
+
+    minutes = int(input("Kindly input any number greater than 60 = "))
+    print(f"You entered {minutes} minutes which is {minutes / 60 } hours.")
+
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
